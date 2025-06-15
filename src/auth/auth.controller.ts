e0cf3ca7cb06a5723297fdf6d3b225abf71e6b6c
wuaho <juanjoserequenalama@gmail.com>
2025-06-15 17:47:30 +0900

    Add route to login through google auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import {
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GoogleAuthGuard } from './guards/google-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,6 +23,22 @@ export class AuthController {
     return this.authService.signIn(req.user);
   }
 
+  @UseGuards(GoogleAuthGuard)
+  @Get('google')
+  async googleAuth() {
+    console.log('Holap esto es el google auth');
+  }
+
+  //TODO: i believe that the redirect should be the frontend url right? or should i
+  // redirect the frontend from the backend?
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/redirect')
+  async googleAuthRedirect(@Request() req): Promise<{ access_token: string }> {
+    return this.authService.signIn(req.user);
+  }
+
+  // TODO: move this method to the users controller
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
